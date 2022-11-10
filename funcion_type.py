0cbe2cfd0d67730a0dad183c1c3f5906cf23153c
WalterRiveraGT <walterej@hotmail.es>
2022-11-09 22:38:06 -0600

    agregando archivito de repaso sobre la función type

---
+++ funcion_type.py
@@ -0,0 +1,8 @@
+##Función type:
+##Sirve para identificar el tipo de dato  de una variable
+numeric=1
+print(type(numeric))
+print('tipo de dato cadena de texto:   ')
+print(type('hola'))
+print('tipo de dato cadena booleano:   ')
+print(type(True))
