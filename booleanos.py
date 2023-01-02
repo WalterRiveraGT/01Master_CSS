008cf37406f1fde6820859c7ad6f1ef433b00b18
WalterRiveraGT <walterej@hotmail.es>
2023-01-02 00:12:04 -0600

    eliminando datos copiados accidentalmente

--- booleanos.py
+++
@@ -1,8 +0,0 @@
-print(True)
-print (False)
-
-
-for contador in range(10):
-    if contador!=10:
-        contador=contador+1
-        print(contador)
--- funcion_type.py
+++
@@ -1,8 +0,0 @@
-##Función type:
-##Sirve para identificar el tipo de dato  de una variable
-numeric=1
-print(type(numeric))
-print('tipo de dato cadena de texto:   ')
-print(type('hola'))
-print('tipo de dato cadena booleano:   ')
-print(type(True))
